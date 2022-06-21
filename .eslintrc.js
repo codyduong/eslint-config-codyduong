248e7cb5c376961bd26033d9f19a53fd53b1ac22
Cody Duong <duongcody507@gmail.com>
2022-06-21 02:01:08 -0500

    init

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'eslint-plugin-prettier'],
+  rules: {
+    indent: ['off', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['off', 'unix'],
+    quotes: ['off', 'double'],
+    semi: ['error', 'always'],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.js'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+        ],
+        '@typescript-eslint/explicit-function-return-type': ['warn'],
+        'no-empty-function': 'warn',
+        '@typescript-eslint/no-empty-function': ['warn'],
+      },
+    },
+  ],
+};
---
+++ README.md
@@ -0,0 +1,8 @@
+My personal eslint config
+
+Install with
+```sh
+yarn add -D 'https://github.com/codyduong/eslint-config-codyduong.git'
+# or with semver
+yarn add -D 'https://github.com/codyduong/eslint-config-codyduong.git#semver:v1.0.0'
+```
